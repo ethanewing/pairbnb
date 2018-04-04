2b33e6722e26b4d2118ec94d8b38b1da8893303d
Ethan Ewing <ethanewing52@gmail.com>
2018-04-04 11:48:00 +0800

    installed Figaro Gem

--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -1,5 +1,6 @@
 Clearance.configure do |config|
   config.routes = false
   config.mailer_sender = "reply@example.com"
+  config.password_strategy = Clearance::PasswordStrategies::BCrypt
   config.rotate_csrf_on_sign_in = true
 end
